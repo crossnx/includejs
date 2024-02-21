fca68b81d029f137c1420722b9e67623e163161c
Tony Gorez <gorez.tony@gmail.com>
2024-02-21 20:48:07 +0100

    feat(value): add null (#35)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

---
+++ test/engine/engine_value_null_test.cc
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include <includejs/engine.h>
+
+TEST(IncludeJS_Engine, create_null) {
+  sourcemeta::includejs::Engine engine;
+
+  auto null = engine.context().from(nullptr);
+  EXPECT_TRUE(null.is_null());
+}
+
+TEST(IncludeJS_Engine, evaluate_null) {
+  sourcemeta::includejs::Engine engine;
+  sourcemeta::includejs::Value result{engine.evaluate("null", "index.js")};
+  EXPECT_TRUE(result.is_null());
+}
