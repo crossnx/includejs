64d4173212a1f8db06d4a24b37f6030a62a710cf
Tony Gorez <gorez.tony@gmail.com>
2024-02-22 09:55:56 +0100

    feat(value): add is_function
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

---
+++ test/engine/engine_value_function_test.cc
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include <includejs/engine.h>
+
+TEST(IncludeJS_Engine, is_function) {
+  sourcemeta::includejs::Engine engine;
+
+  auto obj = engine.context().make_object();
+  obj.set(
+      "foo",
+      [](std::vector<sourcemeta::includejs::Value> arguments)
+          -> sourcemeta::includejs::Value { return std::move(arguments[0]); });
+  obj.set("bar", engine.context().from(42));
+
+  EXPECT_TRUE(obj.at("foo").has_value());
+  EXPECT_TRUE(obj.at("foo").value().is_function());
+
+  EXPECT_TRUE(obj.at("bar").has_value());
+  EXPECT_FALSE(obj.at("bar").value().is_function());
+}
