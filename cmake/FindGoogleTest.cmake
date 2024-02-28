671dedc6f49dff98189874099837d3074eb5dcfd
Tony Gorez <gorez.tony@gmail.com>
2024-02-28 21:04:55 +0100

    ci: enable asan and ubs (#26)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- cmake/FindGoogleTest.cmake
+++ cmake/FindGoogleTest.cmake
@@ -5,3 +5,9 @@ if(NOT GoogleTest_FOUND)
   include(GoogleTest)
   set(GoogleTest_FOUND ON)
 endif()
+
+# We want to disable the unsigned shift base sanitizer for gtest
+# because it triggers on the gtest code.
+if(INCLUDEJS_UNDEFINED_SANITIZER)
+  target_compile_options(gtest PRIVATE "-fno-sanitize=unsigned-shift-base")
+endif()
