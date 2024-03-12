fbc376e516f50e2b62c76577a1c9ee4791f17eac
Robert Günzler <r@gnzler.io>
2024-03-12 15:54:28 +0100

    Support selecting JavaScriptCore API version
    Signed-off-by: Robert Günzler <r@gnzler.io>

--- cmake/FindJavaScriptCore.cmake
+++ cmake/FindJavaScriptCore.cmake
@@ -8,7 +8,26 @@ if(NOT JavaScriptCore_FOUND)
   elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     add_library(javascript_core INTERFACE IMPORTED)
     find_package(PkgConfig REQUIRED)
-    pkg_check_modules(JAVASCRIPTCORE REQUIRED javascriptcoregtk-4.0)
+
+    # If find_package(JavaScriptCore [version]) is called we only look for that
+    # otherwise loop through a list of supported options, pick the first one found
+    # and error out if there is no match
+    if(JavaScriptCore_FIND_VERSION)
+      pkg_check_modules(JAVASCRIPTCORE javascriptcoregtk-${JavaScriptCore_FIND_VERSION} REQUIRED)
+    else()
+      set(supported_versions 6.0 4.1 4.0)
+      foreach(ver ${supported_versions})
+        pkg_check_modules(JAVASCRIPTCORE javascriptcoregtk-${ver})
+        if(JAVASCRIPTCORE_FOUND)
+          set(JavaScriptCore_FIND_VERSION ${ver})
+          break()
+        endif()
+      endforeach()
+      if(NOT JAVASCRIPTCORE_FOUND)
+        message(FATAL_ERROR "Required package JavaScriptCore was not found")
+      endif()
+    endif()
+
     set_property(TARGET javascript_core PROPERTY
       INTERFACE_INCLUDE_DIRECTORIES ${JAVASCRIPTCORE_INCLUDE_DIRS})
     set_property(TARGET javascript_core PROPERTY
@@ -17,6 +36,20 @@ if(NOT JavaScriptCore_FOUND)
       INTERFACE_LINK_OPTIONS ${JAVASCRIPTCORE_LDFLAGS})
     set_property(TARGET javascript_core PROPERTY
       INTERFACE_LINK_LIBRARIES ${JAVASCRIPTCORE_LINK_LIBRARIES})
+
+    if(JavaScriptCore_FIND_VERSION STREQUAL "6.0")
+      # NOTE: this is required since WebkitGTK (which includes JavaScriptCore) only
+      # exposes the GLib headers starting in versions after 4.0
+      # see: https://github.com/WebKit/WebKit/blob/main/Source/WebKit/gtk/migrating-to-webkitgtk-6.0.md#stop-using-deprecated-apis
+      FILE(GLOB headers ${PROJECT_SOURCE_DIR}/vendor/webkitgtk/Source/JavaScriptCore/API/*.h)
+      FOREACH(header ${headers})
+        cmake_path(GET header FILENAME header_filename)
+        configure_file("${header}" "${PROJECT_BINARY_DIR}/javascriptcore-headers/JavaScriptCore/${header_filename}" COPYONLY)
+      ENDFOREACH()
+      set_property(TARGET javascript_core PROPERTY
+        INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_BINARY_DIR}/javascriptcore-headers")
+    endif()
+
     add_library(JavaScriptCore::JavaScriptCore ALIAS javascript_core)
     set(JavaScriptCore_FOUND ON)
   endif()
