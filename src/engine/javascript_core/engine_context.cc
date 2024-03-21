7a817fe38252e52b9c7fea411b28eaf6d0c57971
Tony Gorez <gorez.tony@gmail.com>
2024-03-21 15:10:24 +0100

    fix: include stdexcept
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- src/engine/javascript_core/engine_context.cc
+++ src/engine/javascript_core/engine_context.cc
@@ -6,6 +6,7 @@ extern "C" {
 }
 
 #include <cassert> // assert
+#include <stdexcept>
 
 namespace includejs {
 
--- test/engine/engine_context_from_json_test.cc
+++ test/engine/engine_context_from_json_test.cc
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
