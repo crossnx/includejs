9b11ace04785a9d45301667ad38ab97ca1855a15
Balakrishna Avulapati <ba@bavulapati.com>
2025-12-26 19:24:11 +0530

    update javascriptcoregtk to 4.1
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- cmake/FindJavaScriptCore.cmake
+++ cmake/FindJavaScriptCore.cmake
@@ -8,7 +8,7 @@ if(NOT JavaScriptCore_FOUND)
   elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     add_library(javascript_core INTERFACE IMPORTED)
     find_package(PkgConfig REQUIRED)
-    pkg_check_modules(JAVASCRIPTCORE REQUIRED javascriptcoregtk-4.0)
+    pkg_check_modules(JAVASCRIPTCORE REQUIRED javascriptcoregtk-4.1)
     set_property(TARGET javascript_core PROPERTY
       INTERFACE_INCLUDE_DIRECTORIES ${JAVASCRIPTCORE_INCLUDE_DIRS})
     set_property(TARGET javascript_core PROPERTY
